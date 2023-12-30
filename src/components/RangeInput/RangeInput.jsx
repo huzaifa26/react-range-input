194ef480bc8b11c8e62d9ceb604f9af276862638
huzaifa26 <abidhuzaifa128@gmail.com>
2023-12-30 15:34:51 +0500

    updates

--- src/components/RangeInput/RangeInput.jsx
+++ src/components/RangeInput/RangeInput.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './style.css'
 
-export function RangeInput({ updateRating, width = 400, minValue = 1, maxValue = 10, step = 1, value = 0 }) {
+export function RangeInput({ updateRating, width = 400, minValue = 0, maxValue = 100, step = 20 }) {
   const [rangeValue, setRangeValue] = useState(minValue);
 
   useEffect(() => {
@@ -13,11 +13,6 @@ export function RangeInput({ updateRating, width = 400, minValue = 1, maxValue =
     updateRating(event.target.value);
   }
 
-  // const trackStyle = {
-  //   '--progress': `${rangeValue * 10}%`,
-  //   background: `linear-gradient(to right, #777, #777 var(--progress), #777 var(--progress), #777)`
-  // };
-
   const trackStyle = {
     '--progress': `${rangeValue * 10}%`,
     background: `linear-gradient(to right, #777, #777 var(--progress), #777 var(--progress), #777)`,
@@ -27,7 +22,7 @@ export function RangeInput({ updateRating, width = 400, minValue = 1, maxValue =
     width: '1.25vh',
     height: '1.25vh',
     backgroundColor: '#777',
-    borderRadius: '50%', // rounded-full in Tailwind corresponds to a fully rounded border
+    borderRadius: '50%',
   };
 
   const styleObjectInput = {
@@ -38,46 +33,49 @@ export function RangeInput({ updateRating, width = 400, minValue = 1, maxValue =
     position: 'absolute',
     top: '50%',
     transform: 'translateY(-50%)',
+    margin: 0,
   };
 
   return (
-    <div
-      style={{
-        width,
-        position: 'relative',
-        maxHeight: '2.5vh',
-        minHeight: '2.5vh',
-        background: 'aqua',
-      }}
-    // className='w-full relative max-h-[2.5vh] min-h-[2.5vh]'
-    >
+    <>
       <div
+        className='rangeInput'
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          paddingRight: '0.7vh',
-          paddingLeft: '0.7vh',
-          position: 'absolute',
-          width: '100%',
-          top: '50%',
-          transform: 'translateY(-50%)',
-          zIndex: 10,
+          width,
+          position: 'relative',
+          maxHeight: '2.5vh',
+          minHeight: '2.5vh',
         }}
-      // className='flex justify-between px-[0.7vh] absolute w-full top-[50%] translate-y-[-50%] z-10'
       >
-        {Array.from({ length: maxValue }, (_, index) => index).map((_, index) => (
-          <div style={styleObject}></div>
-        ))}
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            paddingRight: '0.7vh',
+            paddingLeft: '0.7vh',
+            position: 'absolute',
+            width: '100%',
+            top: '50%',
+            transform: 'translateY(-50%)',
+            zIndex: 10,
+            boxSizing: 'border-box',
+          }}
+        >
+          {Array.from({ length: maxValue / step + 1 }, (_, index) => index).map((_, index) => (
+            <div style={styleObject}></div>
+          ))}
+        </div>
+        <input
+          type="range"
+          min={minValue}
+          max={maxValue}
+          step={step}
+          value={rangeValue}
+          onChange={handleRangeChange}
+          style={{ ...trackStyle, ...styleObjectInput }}
+        />
       </div>
-      <input
-        type="range"
-        min={minValue}
-        max={maxValue}
-        // step={step}
-        value={rangeValue}
-        onChange={handleRangeChange}
-        style={{ ...trackStyle, ...styleObjectInput }}
-      />
-    </div>
+      <div>{rangeValue}</div>
+    </>
   )
 }
